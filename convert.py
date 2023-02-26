3409a8766f5b73af92ce13391e22398e436d3a6d
Criid <Lowry_46@hotmail.com>
2023-02-26 16:37:11 -0500

    Changed path input to be able to execute out of cwd

--- convert.py
+++ convert.py
@@ -3,14 +3,20 @@ from pathlib import Path
 import os
 
 #Pulls from the current directory. Will change this to be more versatile in what directory is being used
-path = Path.cwd()
+# path = Path.cwd()
+path = "PATH"
 #Creates a list of all files in chosen directory that have the .webp extension
 webp_files = [f for f in os.listdir(path) if f.endswith(".webp")]
 
 #Loops through all files in list of files, converts them to jpeg and then deletes the webp version out of the directory
 for file in webp_files:
-    image = Image.open(file).convert("RGB")
-    image.save(f"{file.strip('.webp')}.jpeg", "jpeg")
-    os.remove(file)
+    image = Image.open(path + "\\" + file).convert("RGB")
+    try:
+        image.save(f"{path}\\{file.strip('.webp')}.jpeg", "jpeg")
+    except FileNotFoundError:
+        image.save(f"{path}\\{file}.jpeg", "jpeg")
+    os.remove(path + "\\" + file)
+
+
 
 
