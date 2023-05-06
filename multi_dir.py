0721bd9c0f55eb60863a85baa12a12a849bde29b
Criid <Lowry_46@hotmail.com>
2023-05-06 16:43:17 -0400

    chaning path

---
+++ multi_dir.py
@@ -0,0 +1,22 @@
+import os
+from PIL import Image
+
+path = r"PATH"
+
+for root, dirs, files in os.walk(path):
+    for name in files:
+        if name.endswith(".webp"):
+            file_name = os.path.join(root, name)
+            image = Image.open(file_name).convert("RGB")
+            try:
+                image.save(f"{file_name.strip('.webp')}.jpeg", "jpeg")
+            except:
+                print("Failed")
+            os.remove(file_name)
+
+
+
+
+
+
+
